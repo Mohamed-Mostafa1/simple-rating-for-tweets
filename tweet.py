6b143f6217d7f6f3630c9c6bdfd421a516a55bb9
Mohamed Mostafa <mohamed.mostafa1995@hotmail.com>
2018-05-15 07:47:02 +0200

    last edit

--- tweet.py
+++ tweet.py
@@ -7,15 +7,17 @@ consumer_secret = ''
 
 access_token = ''
 access_token_secret = ''
-#---------------------------------
+#---------------------------------------------
+
+# Authintication part
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-
 api = tweepy.API(auth)
 
-public_tweets = api.search('paladins')
+
+public_tweets = api.search('')      # specify the topic you want to get rating for here
 
 
 for tweet in public_tweets:
